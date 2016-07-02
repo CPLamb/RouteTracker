dac088095fb506a99b53acbea7f8566ce6dbbb01
liangh <liangh@balluun.com>
2016-07-02 11:19:11 -0700

    homeview scrollable, filtered results stats on home view stats

--- RouteTracker/HomeViewController.m
+++ RouteTracker/HomeViewController.m
@@ -9,7 +9,7 @@
 /*
  This link talks about breaking up storyboards
  http://www.newventuresoftware.com/blog/organizing-xcode-projects-using-multiple-storyboards/
-*/
+ */
 
 #import "HomeViewController.h"
 #import <CoreLocation/CoreLocation.h>
@@ -25,7 +25,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *returnTextField;
 @property (weak, nonatomic) IBOutlet UITextField *bundleTextField;
 @property (weak, nonatomic) IBOutlet UITextField *copiesTextField;
+@property (weak, nonatomic) IBOutlet UIScrollView *homeScrollView;
 
+@property (copy, nonnull) NSString* searchString;
 @end
 
 @implementation HomeViewController
@@ -41,25 +43,44 @@
         [self.locationManager startUpdatingLocation];
     }
     // Do any additional setup after loading the view.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(newSearchFromList:) name:kListTableStartNewSearchNotification object:nil];
+    
+    // init search string = nil
+    // search string can only set by notification from list view controller
+    // if it have been set, new filter will be enabled.
+    _searchString = nil;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     NSString *dataFilenameIndex = [[NSUserDefaults standardUserDefaults] stringForKey:@"selected_plist"];
-
+    
     NSLog(@"HomeVC -- dataFilenameIndex = %@", dataFilenameIndex.description);
-
-// Sets up display of magazine loaded
+    
+    // Sets up display of magazine loaded
     self.selectedMagazineLabel.text = dataFilenameIndex.description;
     
     NSString *selectedMagazinePhotoName = [[NSUserDefaults standardUserDefaults] stringForKey:@"selected_photo"];
     
     self.selectedMagazineLogo.image = [UIImage imageNamed:selectedMagazinePhotoName];
-
+    
     NSLog(@"HomeVC -- Listing the loaded spreadsheet %@", self.selectedMagazineLabel.text);
     
     [self calculateTotals:[self selectProperPlistData]];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    CGFloat height = _homeScrollView.frame.size.height > 516.0f ?
+    _homeScrollView.frame.size.height + 1.0f : 516.0f;
+    _homeScrollView.contentSize = CGSizeMake(self.view.frame.size.width, height);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -70,30 +91,36 @@
 
 #pragma mark - Navigation
 /*
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 #pragma mark - Custom Methods
 
-- (IBAction)resetLastRead:(UIBarButtonItem *)sender {
+- (void)newSearchFromList: (NSNotification *)notification {
+    if (notification.object) {
+        _searchString = notification.object;
+    }
+}
 
+- (IBAction)resetLastRead:(UIBarButtonItem *)sender {
+    
     // Show UI Effect
     UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Last Read Has Been reset"
                                                                    message:@"Now it is like a new install"
                                                             preferredStyle:UIAlertControllerStyleAlert];
-
+    
     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
                                                           handler:^(UIAlertAction * action) {}];
     
     [alert addAction:defaultAction];
     [self presentViewController:alert animated:YES completion:nil];
-
+    
     [[NSUserDefaults standardUserDefaults]  setObject:NULL forKey:@"dateKey"];
-
+    
     NSLog(@"HomeVC -- LastReadReset to NULL");
 }
 
@@ -106,10 +133,33 @@
     AppDelegate *delegate = [UIApplication sharedApplication].delegate;
     [delegate.memberData loadPlistData];
     
+    NSArray *array = [NSArray arrayWithArray:delegate.memberData.membersArray];
+    
     NSLog(@"HomeVC -- selectProper pList -- Loads fileName %@", myFilename);
-    return [NSArray arrayWithArray:delegate.memberData.membersArray];
+    if (_searchString != nil && _searchString.length > 0) {
+        NSLog(@"Before filter has %ld count", array.count);
+        NSMutableArray *filterArray = [NSMutableArray new];
+        
+        for (int i=0; i<+[array count]-1; i++) {
+            NSString *searchName = [[array objectAtIndex:i] objectForKey:@"Name"];
+            NSString *searchDriver = [[array objectAtIndex:i] objectForKey:@"Driver"];
+            NSString *searchCategory = [[array objectAtIndex:i] objectForKey:@"Category"];
+            
+            BOOL foundInName = [searchName rangeOfString:_searchString options:NSCaseInsensitiveSearch].location == NSNotFound;
+            BOOL foundInDriver = [searchDriver rangeOfString:_searchString options:NSCaseInsensitiveSearch].location == NSNotFound;
+            BOOL foundInCategory = [searchCategory rangeOfString:_searchString options:NSCaseInsensitiveSearch].location == NSNotFound;
+            if (!foundInName || !foundInDriver || !foundInCategory) {
+                [filterArray addObject:[array objectAtIndex:i]];
+            }
+        }
+        
+        NSLog(@"after filter has %ld count", filterArray.count);
+        _searchString = nil;
+        return [NSArray arrayWithArray:filterArray];
+    } else {
+        return array;
+    }
 }
-
 
 - (void) calculateTotals:(NSArray *)array
 {
